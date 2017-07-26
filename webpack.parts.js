947275831cb8f9f2cf8e4d146e9d75dde1cd8174
Juho Vepsalainen <bebraw@gmail.com>
2017-07-26 11:08:29 +0200

    chore: Set up style loading

--- webpack.parts.js
+++ webpack.parts.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 exports.devServer = ({ host, port } = {}) => ({
   devServer: {
     historyApiFallback: true,
@@ -26,3 +28,42 @@ exports.lintJavaScript = ({ include, exclude, options }) => ({
     ],
   },
 });
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
+
+exports.extractCSS = ({ include, exclude, use }) => {
+  // Output extracted CSS to a file
+  const plugin = new ExtractTextPlugin({
+    filename: '[name].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: plugin.extract({
+            use,
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    plugins: [ plugin ],
+  };
+};
